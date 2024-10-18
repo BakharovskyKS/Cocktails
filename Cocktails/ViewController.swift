8dbe46dd95bce8757c38a97fda3cf6bec3929e03
Kirill Bakharovsky <baharovsky@bk.ru>
2024-10-19 00:02:21 +0500

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Кирилл Бахаровский on 10/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CocktailsTests/CocktailsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsTests.swift
+//  CocktailsTests
+//
+//  Created by Кирилл Бахаровский on 10/19/24.
+//
+
+import Testing
+@testable import Cocktails
+
+struct CocktailsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
